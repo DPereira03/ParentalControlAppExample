e98cea7876ca6c2b757eb4c292dde199ee2f43c1
David Pereira <dp433@canterbury.ac.uk>
2024-02-13 14:37:58 +0000

    Encrypts search box text to simulate VPN connection

--- ParentalControlAppExample/Form2.cs
+++ ParentalControlAppExample/Form2.cs
@@ -5,9 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Security;
-using System.Text;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
 
 namespace ParentalControlAppExample
 {
@@ -25,13 +26,61 @@ namespace ParentalControlAppExample
             firstForm.DisplayBox.Text = "HELP NEEDED";
         }
 
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+                byte[] encryptedData;
+ 
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    RSA.ImportParameters(RSAKeyInfo);
+  
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+ 
+                    RSA.ImportParameters(RSAKeyInfo);
+  
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+        }
+
+
         private void SearchBut_Click(object sender, EventArgs e)
         {
             string UserSearch = SearchBar.Text;
 
             if (label1.Text.Contains("VPN"))
             {
-                MessageBox.Show("Yes");
+
+                byte[] dataToEncrypt = Encoding.UTF8.GetBytes(UserSearch);
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                    string OUTDEC = Encoding.UTF8.GetString(decryptedData);
+
+
+                    MessageBox.Show("Decrypted: "+ OUTDEC);
+                }
+
             }
 
             string text = "Showing results for " + UserSearch;
